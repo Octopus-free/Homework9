cdb1249e46404d83393ee51d60ec1529d44eab99
Octopus-free <ivashkinrg@gmail.com>
2020-01-02 23:19:28 +0300

    Final commit

--- GameRunning.py
+++ GameRunning.py
@@ -29,11 +29,8 @@ print_for_game = PrintingCard()
 # для проверок карточек игроков
 checks_for_game = CheckingCard()
 
+# создаем цикл для генерации ходов игры
 for turn_number in range(1, 91):
-
-    # в начале хода проверяем карточки игроков на предмет всех зачеркнутых чисел
-    checks_for_game.check_human_players(human_players_cards)
-    checks_for_game.check_computer_players(computers_players_cards)
 
     # объявляем номер текущего хода
     print(turn_for_game.turn_step(turn_number))
@@ -43,6 +40,7 @@ for turn_number in range(1, 91):
     print(f'Выпал бочонок {current_barrel}')
 
     # отрисовываем в терминале карточки игроков людей
+    # запрашиваем выбор игрока о зачеркивании выпавшего бочонка на карточке
     for human_card in human_players_cards:
         player_number = human_players_cards.index(human_card) + 1
         print_for_game.print_human_cards(player_number, human_card)
@@ -53,10 +51,10 @@ for turn_number in range(1, 91):
         else:
             checks_for_game.strike_numbers_answer_no(player_number, current_barrel, human_card)
 
-
-    # запрашиваем выбор игрока о зачеркивании выпавшего бочонка на карточке
+    # отрисовываем в терминале карточки игроков компьютеров и автоматически зачеркиваем выпавший бочонок
     print_for_game.print_computers_cards(computers_players_cards)
     computers_players_cards = checks_for_game.strike_numbers_for_computers(current_barrel, computers_players_cards)
 
-    #
-
+    # в конце хода проверяем карточки игроков на предмет всех зачеркнутых чисел (победы)
+    checks_for_game.check_human_players(human_players_cards)
+    checks_for_game.check_computer_players(computers_players_cards)
